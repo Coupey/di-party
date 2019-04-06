a87106ccb99a0b1c5a0bca162cde39c2438041dc
Paul Coupe <paul.coupe@digital.homeoffice.gov.uk>
2019-04-06 18:47:59 +0100

    Hooked up RSVP to contact email fields

--- contact.php
+++ contact.php
@@ -4,10 +4,16 @@ $siteTitle = 'Party';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
-//If the form is submitted
-if(isset($_POST['submitted'])) {
+echo 'hello\n';
+echo $_POST['name'].'\n';
+echo $_POST['extrapeople'].'\n';
+echo $_POST['coming'].'\n';
+echo $_POST['message'].'\n';
 
-    $extraPeople = $_POST['$extrapeople'];
+//If the form is submitted
+if(isset($_POST['submit'])) {
+	echo 'in the whatnot';
+    $extraPeople = $_POST['extrapeople'];
     $email = 'party@party.com';
 
 	// require a name from user
@@ -48,4 +54,5 @@ if(isset($_POST['submitted'])) {
 		$emailSent = true;
 	}
 }
+echo 'out the whatnot';
 ?>
